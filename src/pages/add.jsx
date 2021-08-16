5a74938d8b5e6e89a8fb24cc70a0088ee8b78c33
Drive Boy Dog Boy Dirty Numb <ssutherland@eveo.com>
2021-08-16 03:26:05 -0700

    stubb out json structure with 2 sampe questions

--- src/pages/add.jsx
+++ src/pages/add.jsx
@@ -39,7 +39,7 @@ function AddCardPage () {
     return (
         <section>
             <div>
-                <h1>Add New Meet</h1>
+                <h1>Add A New Card</h1>
             </div>
             <FormAddComp onAddNewMeet={addNewMeetHandler} />
         </section>
