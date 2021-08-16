1afc28830da6c25a6e75cb094fb92b488013227e
Drive Boy Dog Boy Dirty Numb <ssutherland@eveo.com>
2021-08-16 04:16:11 -0700

    update MeetDetailComp markup templete in meet-detail-comp

--- src/components/meet-detail-comp/meet-detial-comp.jsx
+++ src/components/meet-detail-comp/meet-detial-comp.jsx
@@ -6,18 +6,23 @@ function MeetDetailComp(props) {
     return (
         <li key={props.id}>
             <ContentCardComp>
-            <h3 className='detail-title'>{props.date.month}/{props.date.day} -- {props.title}</h3> 
+            <h3 className='detail-title'>{/*{props.date.month}/{props.date.day}*/}{props.poser}</h3> 
             <div className='detail'>
                 <div className='detail-thumb'>
-                    <img src={props.img} alt={props.title} />
+                    <img src={props.img} alt={props.topic} />
                 </div>
                 <div className='detail-info'> 
-                    <p>{props.desc}</p>
-                    {props.addy}
+                    <p>{props.choices.answer[0]}</p>
+                    <p>{props.choices.answer[1]}</p>
+                    <p>{props.choices.answer[2]}</p>
+                    <p>{props.choices.answer[3]}</p>
+                </div>
+                <div className='detail-info'> 
+                    <p>{props.hint}</p>
                 </div>
                 <div className='detail-action'>
                     <button>
-                        [+]
+                        [+]Topic: {props.topic}
                     </button>
                 </div>
             </div>
