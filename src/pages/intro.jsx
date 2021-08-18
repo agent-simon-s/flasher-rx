0096f4a873747999b45fc726d106052cad26279c
capg-simon-s <simon.sutherland@capgemini.com>
2021-08-18 00:22:51 -0700

    merge & update package.json && add cards to intro && update header and link in nav

--- src/pages/intro.jsx
+++ src/pages/intro.jsx
@@ -1,22 +1,27 @@
 import {Link} from "react-router-dom";
+import ContentCardComp from '../components/content-card/card-comp.jsx';
 
 function IntroPage() {
     return (
         <div>
-            <h1>FlashRX</h1>
-            <p>Welcome to FlashRX</p>
-            <p>Proceed to Quiz <Link to="/all">All</Link></p>
-            <p>Proceed to FAQ <Link to="/faq">faq</Link></p>
-            <p>Add to the pool, <Link to="/add">New Questions Here</Link></p>
-            <p>Planned Improvements </p>
-            <ul>
-                <li>+ History</li>
-                <li>+ Error Tracker</li>
-                <li>+ Average Score</li>
-                <li>+ Resurface Errors</li>
-                <li>+ Account</li>
-                <li>+ login</li>
-            </ul>
+            <ContentCardComp>
+                <h1>FlashRX</h1>
+                <p>Welcome to FlashRX</p>
+                <p>Proceed to Quiz <Link to="/all">All</Link></p>
+                <p>Proceed to FAQ <Link to="/faq">faq</Link></p>
+                <p>Add to the pool, <Link to="/add">New Questions Here</Link></p>
+                <p>Planned Improvements </p>
+            </ContentCardComp>
+            <ContentCardComp>
+                <ul>
+                    <li>+ History</li>
+                    <li>+ Error Tracker</li>
+                    <li>+ Average Score</li>
+                    <li>+ Resurface Errors</li>
+                    <li>+ Account</li>
+                    <li>+ login</li>
+                </ul>
+            </ContentCardComp>
         </div>
     )
 }
