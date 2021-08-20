08f9643188696d6e83e69c0cc27e8079dbf0e0a7
capg-simon-s <simon.sutherland@capgemini.com>
2021-08-19 18:49:55 -0700

    add a quix link

--- src/app.jsx
+++ src/app.jsx
@@ -3,6 +3,7 @@ import './app.css';
 import IntroPage from './pages/intro.jsx';
 import AllMeetUpPage from './pages/all.jsx';
 import FAQPage from './pages/faq.jsx';
+import QuizPage from './pages/quiz.jsx';
 import FavoritesPage from './pages/favorites.jsx';
 import AddCardPage from './pages/add.jsx';
 import LayoutPageDef from './layouts/layout-page-default/layout-page-default.jsx'
@@ -20,6 +21,9 @@ function App() {
            </Route>
           <Route path="/add">
             <AddCardPage></AddCardPage>
+          </Route>
+          <Route path="/quiz">
+            <QuizPage></QuizPage>
           </Route>
           <Route path="/all">
             <AllMeetUpPage></AllMeetUpPage>
