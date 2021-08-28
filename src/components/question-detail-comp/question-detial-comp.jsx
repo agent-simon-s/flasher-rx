aa428b8039718aab9b5070e94f27d5251ddbc4f9
capg-simon-s <simon.sutherland@capgemini.com>
2021-08-28 01:08:50 -0700

    add quiz detail comp

---
+++ src/components/question-detail-comp/question-detial-comp.jsx
@@ -0,0 +1,43 @@
+import ContentCardComp from '../content-card/card-comp.jsx';
+import './meet-detial-comp.css';
+
+
+function MeetDetailComp(props) {
+    return (
+        <li key={props.id}>
+            <ContentCardComp>
+            <h3 className='detail-title'>{/*{props.date.month}/{props.date.day}*/}{props.poser}</h3> 
+            <div className='detail'>
+                <div className='detail-thumb'>
+                    <img src={props.img[0].src} alt={props.img[0].alt} />
+                </div>
+                <div className='detail-info'>
+                    {
+                        props.choices.map((item,index) => {
+                            return(
+                                <p key={index}>{item.text}</p>
+                            )
+                        })
+                    } 
+                </div>
+                {/*<div className='detail-info'> 
+                    <p>{props.choices[0].text}</p>
+                    <p>{props.choices[1].text}</p>
+                    <p>{props.choices[2].text}</p>
+                    <p>{props.choices[3].text}</p>
+                </div>*/}
+                <div className='detail-info'> 
+                    <p>{props.hint}</p>
+                </div>
+                <div className='detail-action'>
+                    <button>
+                        [+]Topic: {props.topic}
+                    </button>
+                </div>
+            </div>
+            </ContentCardComp>
+        </li>
+    )
+}
+
+export default MeetDetailComp;
