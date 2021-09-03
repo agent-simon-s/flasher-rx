bf787e078f461759093bfe104e496bf6ac844f59
capg-simon-s <simon.sutherland@capgemini.com>
2021-09-03 01:17:36 -0700

    flesh out context providor

--- src/store/context-quiz.js
+++ src/store/context-quiz.js
@@ -1,15 +1,49 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
-const ContextFavorites = createContext({
-    favorites: [],
-    totalFavorites: 0
+const ContextQuiz = createContext({
+    answered: [],
+    revealed: [],
+    totalRevealed: 0,
+    totalAnswered: 0,
+    totalCorrect: 0,
+    totalWrong: 0
 });
 
-function ProviderContextFavorites(props) {
+export function ProviderContextQuiz(props) {
+    const [ qRevealed, setQRevealed ] = useState([])
+    //const [ correctAns, setCorrectAns ] = useState([])
+    //const [ wrongAns, setWrongAns ] = useState([])
 
-    const context = {};
+    
+    function addQuizHandler (newQ) {
+        setQRevealed((prevQs) => {
+            return prevQs.concat(newQ);
+        });
+    }
 
-    return <ContextFavorites.Provider value={context}>
-        {props.children}
-    </ContextFavorites.Provider>
+    function removeQuizHandler (myQId) {
+        setQRevealed((prevQs) => {
+            return prevQs.filter( qs => qs.id !== myQId);
+        });
+    }
+
+    function isQsRevealedHandler (myQId) {
+        return qRevealed.some( qs => qs.id === myQId );
+    }
+
+    const context = {
+        revealed: qRevealed,
+        totalRevealed: qRevealed.length,
+        addQuiz: addQuizHandler,
+        isQsAsked: isQsRevealedHandler
+    };
+
+
+    return (
+        <ContextQuiz.Provider value={context}>
+            {props.children}
+        </ContextQuiz.Provider>
+    );
 }
+
+export default ContextQuiz;
