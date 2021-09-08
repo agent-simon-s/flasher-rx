b8ef859069aab05a11189113ff9c02748fd06b2d
capg-simon-s <simon.sutherland@capgemini.com>
2021-09-08 03:12:32 -0700

    expand hompage content

--- src/pages/intro.jsx
+++ src/pages/intro.jsx
@@ -6,20 +6,26 @@ function IntroPage() {
         <div>
             <ContentCardComp>
                 <h1>FlashRX</h1>
-                <p>Welcome to FlashRX</p>
-                <p>Proceed to Quiz <Link to="/all">All</Link></p>
-                <p>Proceed to FAQ <Link to="/faq">faq</Link></p>
-                <p>Add to the pool, <Link to="/add">New Questions Here</Link></p>
-                <p>Planned Improvements </p>
+                <p>Welcome to FlashRX a flashcard inspired study tool.</p>
+                <p>Ready to try <Link to="/all">the Quiz</Link>?</p>
+                <p>If you have questions or want to learn more checkout <Link to="/faq">the F.A.Q.</Link></p>
+                <p>Add questions to the pool, <Link to="/add">New Questions Here</Link></p>
             </ContentCardComp>
             <ContentCardComp>
+            <h3>Planned Improvements </h3>
                 <ul>
-                    <li>+ History</li>
-                    <li>+ Error Tracker</li>
-                    <li>+ Average Score</li>
-                    <li>+ Resurface Errors</li>
-                    <li>+ Account</li>
-                    <li>+ login</li>
+                    <li className="todo">+ Score Card</li>
+                    <li className="todo">+ Result Score</li>
+                    <li className="todo">+ History</li>
+                    <li className="done">+ Error Tracker</li>
+                    <li className="done">+ Resurface Errors</li>
+                    <li className="done">+ Average Score</li>
+                    <li className="done">+ Account</li>
+                    <li className="done">+ login</li>
+                    <li className="todo">+ Test by subject</li>
+                    <li className="todo">+ Dark Mode</li>
+                    <li className="todo">+ High Contrast Mode</li>
+                    <li className="todo">+ Fixed Buttons</li>
                 </ul>
             </ContentCardComp>
         </div>
@@ -27,3 +33,19 @@ function IntroPage() {
 }
 
 export default IntroPage;
+
+// Notes:
+// x Add  myAnswerIs constant
+// x Add check values vs myAnswerIs
+// x Add check & x icons on reveal
+// x Add functn to 'ask' first question in array
+// x reset function
+// _ back button
+// x counter / progress tracker
+// _ score tracker 
+// _ end score tracker 
+// History
+// error tracker
+// error inject
+// Account
+// login
