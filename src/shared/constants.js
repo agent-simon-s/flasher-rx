527a10659313961d0adf6bc8bf0a5e11798ab567
capg-simon-s <simon.sutherland@capgemini.com>
2021-09-18 02:09:03 -0700

    add allt data url

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,1 +1,2 @@
-const FETCH_PATH = `${process.env.__DATA_HOST__}:${process.env.__DATA_PORT__}${process.env.__DATA_PATH__}`
+//const FETCH_PATH = `${process.env.__DATA_HOST__}:${process.env.__DATA_PORT__}${process.env.__DATA_PATH__}`
+export const FETCH_PATH = `/data/db.json`
